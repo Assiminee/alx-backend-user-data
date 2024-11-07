1545aefc87080cdfb964ff0b2ea29b9b9c753a3a
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-11-08 00:12:29 +0100

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,12 @@
 """
 Module containing filter_datum function
 """
+from os import environ
 from typing import List
 import re
 import logging
+
+import mysql
 
 
 def filter_datum(
@@ -49,3 +52,17 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to a MySQL database """
+
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    connector = mysql.connector.connection.MySQLConnection(
+        user=username, password=password, host=host, database=db_name)
+    return connector
+
