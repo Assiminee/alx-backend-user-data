a9591ebc755aa0b02f4f0c5e19a9f0d42408f1f0
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-11-08 22:53:28 +0100

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -53,18 +53,16 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """ Connects to a secure database using credentials from environment variables """
+
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME")
 
-    return mysql.connector.connect(
-        user=username,
-        password=password,
-        host=host,
-        database=db_name
+    return mysql.connector.connection.MySQLConnection(
+        user=username, password=password, host=host, database=db_name
     )
 
 
