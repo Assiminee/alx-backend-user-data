f7b97718b87fabbade1de7343cd97593ad44d591
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-11-15 00:08:43 +0100

    task7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 basic_auth module
 Contains BasicAuth class
 """
+import base64
+
 from api.v1.auth.auth import Auth
 
 
@@ -10,4 +12,17 @@ class BasicAuth(Auth):
     """
     BasicAuth class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the
+        Authorization header for a Basic Authentication
+        """
+        if (
+            not authorization_header or
+            type(authorization_header) != str or
+            not authorization_header.startswith('Basic ')
+        ):
+            return None
+
+        return authorization_header[6:]
