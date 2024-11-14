6ea3341aa7c89b2c06ce699fb95ee2d3bd30a7c5
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-11-15 00:02:45 +0100

    task6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+basic_auth module
+Contains BasicAuth class
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class
+    """
+    pass
